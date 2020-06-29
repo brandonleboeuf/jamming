a5f16ed7f9595544cf80696cf3baf4c45a2658ab
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-29 10:51:03 -0700

    formated react.js

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,4 +1,16 @@
-<div className="SearchResults">
-  <h2>Results</h2>
-  <!-- Add a TrackList component -->
-</div>
+import React from "react";
+import "./SearchResults";
+import { TrackList } from "../TrackList/TrackList";
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList />
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
