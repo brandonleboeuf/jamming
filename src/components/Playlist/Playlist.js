93399e3ffe73fb2caa495bb11767e0053448b581
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-29 10:56:36 -0700

    created component class

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,5 +1,17 @@
-<div class="Playlist">
-  <input value="New Playlist"/>
-  <!-- Add a TrackList component -->
-  <button class="Playlist-save">SAVE TO SPOTIFY</button>
-</div>
+import React from "react";
+import "./Playlist";
+// import { TrackList } from "../TrackList/TrackList";
+
+class Playlist extends React.Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={"New Playlist"} />
+        {/* <TrackList /> */}
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
