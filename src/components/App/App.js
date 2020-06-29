634a6c12ca211d86700374b169334dca338c29da
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-29 10:30:59 -0700

    app.js imports added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,25 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { SearchResults } from "../SearchResults/SearchResults";
+import { Playlist } from "../Playlist/Playlist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        <SearchBar />
+        <div class="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
-{
-  /* <div>
-  <h1>Ja<span class="highlight">mmm</span>ing</h1>
-  <div class="App">
-    <!-- Add a SearchBar component -->
-    <div class="App-playlist">
-      <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component -->
-    </div>
-  </div>
-</div> */
-}
