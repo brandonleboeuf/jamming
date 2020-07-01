03a89cc4f0bc2a10942f05ebc6893a77697ba96b
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-30 18:12:28 -0700

    creat remove track from playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -53,6 +53,7 @@ class App extends React.Component {
       ],
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -62,6 +63,12 @@ class App extends React.Component {
     }
 
     tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
     this.setState({ playlistTracks: tracks });
   }
 
@@ -81,6 +88,7 @@ class App extends React.Component {
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
             />
           </div>
         </div>
