a29818965cca4ed2ac017bbf9fb9b901df27f690
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-01 09:35:54 -0700

    bind search to this

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,6 +54,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
