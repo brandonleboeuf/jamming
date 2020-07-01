16c23fbc9bcf3941c22a32bc3dc17e65ab6746ac
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-07-01 10:37:13 -0700

    created Spotify logic to obtain user access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,31 @@
-import React from "react";
+import id from "./pw";
 
-class Spotify extends React.Component {}
+let accessToken = "";
+
+const redirectURI = "http://localhost:3000/";
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+    // check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      // This clears the parameters, allowing us to grab a new access token once it expires
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      window.location(
+        `https://accounts.spotify.com/authorize?client_id=${id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+      );
+    }
+  },
+};
 
 export default Spotify;
